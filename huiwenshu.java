87ddd116bc99c4a4258f80a34dd71d3576209b13
CJCJH11 <148872418+CJCJH11@users.noreply.github.com>
2023-11-03 23:41:52 +0800

    Add files via upload

---
+++ huiwenshu.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class huiwenshu {
+    public static void main(String[] args) {
+        boolean m=true;
+        int a,b,c;
+        Scanner input = new Scanner(System.in);
+        System.out.println("x =");
+        int x= input.nextInt();
+        c=x%10;
+        if (c==0)
+        {
+         m=false;
+        }
+
+    //写不来了后面
+
+
+
+
+
+
+
+
+
+
+        System.out.println(m);
+    }
+}
